a22320beb0f902d400cd24ba85a6083222f46174
Jeremy D. Frens <jdfrens@gmail.com>
2012-05-29 10:40:05 -0500

    Upgrade yasnippets; move personal snippets to conventional directory; delete unnecessary configuration.

--- customizations/yasnippets.el
+++ customizations/yasnippets.el
@@ -2,6 +2,3 @@
 
 (require 'yasnippet)
 (yas/initialize)
-(setq yas/root-directory '("~/.emacs.d/mysnippets"
-                           "~/.emacs.d/elpa/yasnippet-0.6.1/snippets"))
-(mapc 'yas/load-directory yas/root-directory)
