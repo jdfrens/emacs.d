89bcd49a6e9f11f903434a2f9b1bce0e4ef8f4b5
Jeremy D. Frens <jdfrens@gmail.com>
2012-07-27 13:43:59 -0500

    No autofill with markdown mode; use long-line pseudo-wrapping.

--- customizations/markdown.el
+++ customizations/markdown.el
@@ -3,6 +3,14 @@
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
 
+(defun my-markdown-mode-hook ()
+  (auto-fill-mode "off")
+  (longlines-mode)
+  (longlines-show-hard-newlines)
+  )
+
+(add-hook 'markdown-mode-hook 'my-markdown-mode-hook)
+
 ;; auto mode
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
