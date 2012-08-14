89a4606ad415fb4653457e70d5be138892877f06
Jeremy D. Frens <jdfrens@gmail.com>
2012-08-14 11:40:52 -0500

    Use C-c t t to recompile, not C-c C-c.

--- customizations/compilation.el
+++ customizations/compilation.el
@@ -1,5 +1,5 @@
 ;; ---- compilation mode
-(global-set-key (kbd "C-c C-c") 'recompile)
+(global-set-key (kbd "C-c t t") 'recompile)
 
 (defadvice compile (around set-default-directory (command &optional comint))
   "Use the textmate default directory"
