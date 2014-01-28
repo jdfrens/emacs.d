60230fffe275fc8657679e9eaebf23a928c681fa
Jeremy D. Frens <jdfrens@gmail.com>
2014-01-28 15:24:41 -0600

    Function to toggle between underscore_case and CamelCase.

--- customizations/general.el
+++ customizations/general.el
@@ -2,7 +2,7 @@
 (defun development-windows ()
   "setup the windows I like for development"
   (interactive)
-  (set-frame-size (selected-frame) 200 65)
+  (set-frame-size (selected-frame) 200 56)
   (split-window-right)
   (split-window-below)
   )
@@ -18,3 +18,32 @@
 ;; ---- autopair settings
 (require 'autopair)
 (setq autopair-autowrap t)
+
+
+;; https://www.bunkus.org/blog/2009/12/switching-identifier-naming-style-between-camel-case-and-c-style-in-emacs/
+(defun mo-toggle-identifier-naming-style ()
+  "Toggles the symbol at point between C-style naming,
+e.g. `hello_world_string', and camel case,
+e.g. `HelloWorldString'."
+  (interactive)
+  (let* ((symbol-pos (bounds-of-thing-at-point 'symbol))
+         case-fold-search symbol-at-point cstyle regexp func)
+    (unless symbol-pos
+      (error "No symbol at point"))
+    (save-excursion
+      (narrow-to-region (car symbol-pos) (cdr symbol-pos))
+      (setq cstyle (string-match-p "_" (buffer-string))
+            regexp (if cstyle "\\(?:\\_<\\|_\\)\\(\\w\\)" "\\([A-Z]\\)")
+            func (if cstyle
+                     'capitalize
+                   (lambda (s)
+                     (concat (if (= (match-beginning 1)
+                                    (car symbol-pos))
+                                 ""
+                               "_")
+                             (downcase s)))))
+      (goto-char (point-min))
+      (while (re-search-forward regexp nil t)
+        (replace-match (funcall func (match-string 1))
+                       t nil))
+      (widen))))
